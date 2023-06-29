ddd476f1624b27f88be0ec65d90d3ea1665a0103
Roshini R <roshinisrikrishna@gmail.com>
2023-06-30 00:29:35 +0530

    changes server

--- server/index.js
+++ server/index.js
@@ -332,7 +332,8 @@ else
   
   // Schedule the data fetching and storing process to run every hour (adjust as needed)
   // Start the server
-  app.listen(process.env.PORT||5000, () => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
   console.log('Server running on port 5000');
   });
   
